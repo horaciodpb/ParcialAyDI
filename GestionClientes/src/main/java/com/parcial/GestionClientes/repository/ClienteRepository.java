f3a8eec01756f40366d2e03185c9ed6ccf346f4c
horaciodpb <116599265+horaciodpb@users.noreply.github.com>
2023-09-12 20:08:01 -0300

    Delete GestionClientes directory

--- GestionClientes/src/main/java/com/parcial/GestionClientes/repository/ClienteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.parcial.GestionClientes.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;  
-import com.parcial.GestionClientes.model.Cliente;         
-
-// ClienteRepository.java
-public interface ClienteRepository extends JpaRepository<Cliente, String> {
-}
